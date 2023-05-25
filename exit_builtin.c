805c987a3e40ee494f041aa8520ec6e7ee7e7312
JamesMathenge <mathengejames64@gmail.com>
2023-05-25 13:14:17 +0000

    task 8

--- exit_builtin.c
+++ exit_builtin.c
@@ -7,13 +7,23 @@
 
 void _exit_builtin(char *arg)
 {
-	int status;
-
 	if (arg == NULL)
 		exit(EXIT_SUCCESS);
+	else if (strcmp(arg, "SUCCESS") == 0)
+	{
+		exit(EXIT_SUCCESS);
+	}
+	else if (_strcmp(arg, "FAILURE") == 0)
+	{
+		exit(EXIT_FAILURE);
+	}
+	else if (strcmp(arg, "ERROR") == 0)
+	{
+		exit(1);
+	}
 	else
 	{
-		status = atoi(arg);
-		exit(status);
+		write(STDERR_FILENO, arg, _strlen(arg));
+		exit(1);
 	}
 }
